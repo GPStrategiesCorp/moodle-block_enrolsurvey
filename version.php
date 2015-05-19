5dd991a3c3a024aedb65e0cc32049fc5c839cbc3
Brent Boghosian <brent.boghosian@remote-learner.net>
2015-05-19 13:44:38 -0400

    ELIS-9149: Add missing capabilities/language-strings for ELIS block components.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015010400;
+$plugin->version = 2015010401;
 $plugin->release = '2.8.1.0 (Build: 20150104)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
